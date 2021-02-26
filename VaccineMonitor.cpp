812733c040fafc85eb79f7b03bcec18bf5bb96b3
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-02-26 22:40:21 +0200

    commit

--- VaccineMonitor.cpp
+++ VaccineMonitor.cpp
@@ -7,32 +7,29 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "                             --- THE VACCINE MONITOR PROGRAM STARTED ---                                 " << endl;
-    // ifstream file(argv[2]); // opening the file
-    // string line;            // will store one line of the file each time
-
-    // while (getline(file, line)) // while we have lines in the file, get the line and store it in the variable line
-    // {
-    //     // process string ...
-    //     cout << line << endl;
-    // }
     string extensionOfFileName = (".txt");
     string fileName;
     int bloomSize;
     string line; // will store one line of the file each time
 
     ifstream name;
-    if (argv[1] == "-c")
+    if (string(argv[1]) == "-c") // The form is : ./vaccineMonitor -c citizenRecordsFile –b bloomSize
     {
         fileName = argv[2]; // storing the file name in filename variable
         bloomSize = atoi(argv[4]);
     }
-    else if (argv[3] == "-c")
+    else if (string(argv[3]) == "-c") // The form is : ./vaccineMonitor  –b bloomSize -c citizenRecordsFile
     {
         fileName = argv[4]; // storing the file name in filename variable
         bloomSize = atoi(argv[2]);
     }
+    else
+    {
+        cout << "Please use the right form for the execution command ! " << endl;
+        return 0;
+    }
 
-        fileName.append(extensionOfFileName); // adding extension to the filename
+    fileName.append(extensionOfFileName); // adding extension to the filename
     name.open(fileName);
 
     while (getline(name, line)) // while we have lines in the file, get the line and store it in the variable line
