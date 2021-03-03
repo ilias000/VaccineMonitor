7e9ac44e7c6c4318b634eea34e055649a8bda63e
ilias000 <iliaspiot@yahoo.gr>
2021-03-03 22:31:25 +0200

    commit

--- Script/Script.sh
+++ Script/Script.sh
@@ -3,9 +3,9 @@
 countriesNamesFile=$1
 virusesNamesFile=$2
 maxRecordsNumber=$3
-duplicatesAllowed=$4 # 1 true 
+duplicatesAllowed=$4 # 1 = true, 0 = False
 
-#diavazoume apo ta arxeiia kai ta vazoume stous pinakews
+#read from the file that contains the countries, the countries and inserting them to the array
 declare -a countriesArray;
 
 while IFS= read -r line; do
@@ -19,18 +19,55 @@ while IFS= read -r line; do
 done < $virusesNamesFile
 
 
+declare -a idesArray;
+
 #ftiaxnw ta records
 for (( i=0; i<$maxRecordsNumber; i++ )) do
-    firstNameLength=((3 + $RANDOM % 12))
-    lastNameLength=((3 + $RANDOM % 12))
+    firstNameLength=$((3 + $RANDOM % 12))
+    lastNameLength=$((3 + $RANDOM % 12))
     age=$((1 + $RANDOM % 120))
     firstName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$firstNameLength)
     lastName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$lastNameLength)
     countriesArrayLength=${#countriesArray[@]}
     virusesArrayLength=${#virusesArray[@]}
-    randomCountryIndex=((0 + $RANDOM % $countriesArrayLength))
-    randomVirusIndex=((0 + $RANDOM % $virusesArrayLength))
+    randomCountryIndex=$(($RANDOM % $countriesArrayLength))
+    randomVirusIndex=$(($RANDOM % $virusesArrayLength))
     country=${countriesArray[randomCountryIndex]}
     virus=${virusesArray[randomVirusIndex]}
-    echo $firstName $lastName $country $age $virus >> citizenRecordsFile.txt
+    hasDoneVaccine=$(($RANDOM % 2))
+    date="$((1 + $RANDOM % 30))-$((1 + $RANDOM % 12))-$((2010 + $RANDOM % 10))"
+
+    duplicate=0
+    if [ $duplicatesAllowed -eq 1 ]
+    then
+        duplicate=$(($RANDOM % 2))
+    fi
+
+    lengthIdesArray=${#idesArray[@]}
+    if [ $duplicate -eq 0 ]
+    then
+        isUnique=1
+        while [ $isUnique -ne 0 ]
+        do
+            # id=$((1 + $RANDOM % 9999))
+            # for (( k=0; k<$lengthIdesArray; k++ )) do
+            #     if [ $id -eq ${idesArray[k]} ]
+            #     then
+            #         isUnique=0
+            #         break
+            #     fi
+            # done
+        done
+    else
+        randomId=$(($RANDOM % $lengthIdesArray))
+        id=${idesArray[randomId]}
+    fi
+    
+    idesArray+=("$id")
+    if [ $hasDoneVaccine -eq 0 ]
+    then
+        echo $id $firstName $lastName $country $age "NO" $virus >> ../main/citizenRecordsFile.txt
+    else
+        echo $id $firstName $lastName $country $age "YES" $virus $date >> ../main/citizenRecordsFile.txt
+    fi
 done
