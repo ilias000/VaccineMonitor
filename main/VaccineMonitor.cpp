98d248cbf0973e8291a74432620deef42041d421
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-03-04 00:12:09 +0200

    commit

--- main/VaccineMonitor.cpp
+++ main/VaccineMonitor.cpp
@@ -48,13 +48,22 @@ int main(int argc, char *argv[])
         if (wordsOfLine == NULL)
             return 0;
 
-        for (int i = 0; i < numWords; i++) // printing the array of strings
-            cout << wordsOfLine[i] << endl;
+        // for (int i = 0; i < numWords; i++)
+
+        CitizenRecord *citizen;
+
+        if (numWords == 8)
+            citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], wordsOfLine[7]);
+        else
+            citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], "");
+
+        citizen->print();
 
         /* TODO για καθε γραμμη να αρχικοποιεις και να αποθηκευεις στη μνημη τις δομες δεδομενων 
             που θα χρησιμοποιεισεις κατα την εκτελεση των ερωτηματων*/
 
-        cout << endl;
+        cout
+            << endl;
     }
 
     cout << "                             --- THE VACCINE MONITOR PROGRAM ENDED ---                                 " << endl;
