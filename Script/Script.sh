21061ed2e97f6c14f40c7ee8f58673ad4ecf808b
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-03-04 20:49:16 +0200

    commit

--- Script/Script.sh
+++ Script/Script.sh
@@ -1,33 +1,36 @@
 #! /bin/bash
 
-countriesNamesFile=$1
-virusesNamesFile=$2
-maxRecordsNumber=$3
-duplicatesAllowed=$4 # 1 = true, 0 = False
+countriesNamesFile=$1 # taking the first argumnet and saving it to the countriesNamesFile
+virusesNamesFile=$2 # taking the second argumnet and saving it to the virusesNamesFile
+maxRecordsNumber=$3 # taking the third argumnet and saving it to the maxRecordsNumber
+duplicatesAllowed=$4 # taking the fourth argumnet and saving it to the maxRecordsNumber (1 = true, 0 = False)
 
-#read from the file that contains the countries, the countries and inserting them to the array
-declare -a countriesArray;
 
-while IFS= read -r line; do
+declare -a countriesArray; # creating an array to store coutries names
+
+while IFS= read -r line; do # read from the file that contains the countriesNames, the countries names and inserting them to the array
     countriesArray+=("$line")
 done < $countriesNamesFile
 
-declare -a virusesArray;
+declare -a virusesArray; # creating an array to store viruses names
 
-while IFS= read -r line; do
+while IFS= read -r line; do # read from the file that contains the virusesNames, the viruses names and inserting them to the array
     virusesArray+=("$line")
 done < $virusesNamesFile
 
 
-declare -a idesArray;
+declare -a idesArray; # creating an array to store the ides i use so i know if i have already use an id 
 
-#ftiaxnw ta records
-for (( i=0; i<$maxRecordsNumber; i++ )) do
-    firstNameLength=$((3 + $RANDOM % 12))
-    lastNameLength=$((3 + $RANDOM % 12))
-    age=$((1 + $RANDOM % 120))
-    firstName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$firstNameLength)
-    lastName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$lastNameLength)
+# creating the records
+for (( i=0; i<$maxRecordsNumber; i++ )) do # for as many records as the user wants
+
+    firstNameLength=$((3 + $RANDOM % 10)) # random number [3, 12] for the length of the first name
+    lastNameLength=$((3 + $RANDOM % 10)) # random number [3, 12] for the length of the last name
+    age=$((1 + $RANDOM % 120)) # random number [1, 120] for the length of the last name
+
+    firstName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$firstNameLength) # taking firstNameLength random numbers to create the firstName
+    lastName=$(head -3 /dev/urandom | tr -cd '[:alpha:]' | cut -c -$lastNameLength) # taking LastNameLength random numbers to create the lastName
+    
     countriesArrayLength=${#countriesArray[@]}
     virusesArrayLength=${#virusesArray[@]}
     randomCountryIndex=$(($RANDOM % $countriesArrayLength))
