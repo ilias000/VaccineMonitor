dcfdfb60c3d43ef92b2575da78b65d3fe55dd29c
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-03-06 00:08:59 +0200

    commit

--- General/VaccineMonitor.cpp
+++ General/VaccineMonitor.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <fstream>
+#include <cstring>
+#include <sys/stat.h>
 
 #include <iostream>
 using namespace std;
@@ -38,30 +40,43 @@ int main(int argc, char *argv[])
     }
 
     fileName.append(extensionOfFileName); // adding extension to the filename
-    ifstream name;                        // opening the file that contains the citizen Records
+    struct stat buffer;
+
+    int fileNameLength = fileName.length();
+    char *fileNameChar = new char[fileNameLength + 1];
+    strcpy(fileNameChar, fileName.c_str());
+    if (stat(fileNameChar, &buffer) != 0) // checking if the file exist
+    {
+        cout << "The file : " << fileName << "does not exist!" << endl;
+        delete fileNameChar;
+        return 0;
+    }
+    delete fileNameChar;
+
+    ifstream name; // opening the file that contains the citizen Records
     name.open(fileName);
 
     while (getline(name, line)) // while we have lines in the file, get the line and store it in the variable line
     {
-        // int numWords;                                          // will store the size of the array that I keep every word of the line
-        // string *wordsOfLine = stringSeperator(line, numWords); // wordsOfLine will point to the array of strings that contains every word of the line
-        // if (wordsOfLine == NULL)
-        //     return 0;
+        int numWords;                                            // will store the size of the array that I keep every word of the line
+        string *wordsOfLine = stringSeperator(&line, &numWords); // wordsOfLine will point to the array of strings that contains every word of the line
+        if (wordsOfLine == NULL)
+            return 0;
 
         // for (int i = 0; i < numWords; i++)
         //     cout << wordsOfLine[i] << " ";
         // cout << endl;
-        // CitizenRecord *citizen; // creating a citizen
+        CitizenRecord *citizen; // creating a citizen
 
-        // if (numWords == 8) // if it has 8 it means it has also date
-        //     citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], wordsOfLine[7]);
-        // else // it has not date
-        //     citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], "");
+        if (numWords == 8) // if it has 8 it means it has also date
+            citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], wordsOfLine[7]);
+        else // it has not date
+            citizen = new CitizenRecord(stoi(wordsOfLine[0]), wordsOfLine[1], wordsOfLine[2], wordsOfLine[3], stoi(wordsOfLine[4]), wordsOfLine[5], wordsOfLine[6], "");
 
-        // citizen->print();
-        // cout << endl;
-        // delete[] wordsOfLine;
-        // delete citizen;
+        citizen->print();
+        cout << endl;
+        delete[] wordsOfLine;
+        delete citizen;
     }
 
     cout << "                             ---  THE VACCINE MONITOR PROGRAM ENDED  ---                                 " << endl;
