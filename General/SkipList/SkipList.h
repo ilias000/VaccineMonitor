70e2588adba2d83f961066b93953f8b9c54c87e4
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-04-02 22:48:10 +0300

    commit

---
+++ General/SkipList/SkipList.h
@@ -0,0 +1,43 @@
+#ifndef BLOOMFILTER_H
+#define BLOOMFILTER_H
+
+#include "../LinkedList/LinkedListVaccine/LinkedListVaccine.h"
+
+#include <iostream>
+
+using namespace std;
+
+class BloomFilter
+{
+    int size; // bits of bloom filter
+    char *filter;
+
+public:
+    BloomFilter(int);
+    ~BloomFilter();
+
+    void setBit0(int);                 // finds the K bit and sets it to 0
+    void setBit1(int);                 // finds the K bit and sets it to 1
+    int getBit(int);                   // returns the value of the specified bit
+    int getSize() { return size * 8; } // returns the size of the filter in bits
+    void insert(int);                  // takes the id of the citizen and makes 16 bits of the filter that the hash function will tell 1
+    bool find(int);                    // takes the id and returns true if all of the bits that the hash function will tell are 1
+};
+
+class LinkedListBloomFilter
+{
+
+    LinkedListStringNode *virus;
+    BloomFilter *filter;
+    LinkedListBloomFilter *next;
+
+public:
+    LinkedListBloomFilter(LinkedListStringNode *, int);
+    ~LinkedListBloomFilter();
+
+    void insert(LinkedListStringNode *, int);               // creates a linked list bloom filter with filter for the specified virus and inserts it to the list
+    BloomFilter *getFilter(LinkedListStringNode *);         // returns the filter of the specified virus
+    LinkedListBloomFilter *getNode(LinkedListStringNode *); // returns the node of the specified virus
+};
+
+#endif
