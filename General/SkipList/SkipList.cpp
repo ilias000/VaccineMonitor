959958c64ed4184a913ca5430751c6ba29c27539
Ilias Piotopoulos <iliaspiot@yahoo.gr>
2021-04-05 23:40:46 +0300

    commit

--- General/SkipList/SkipList.cpp
+++ General/SkipList/SkipList.cpp
@@ -1,7 +1,5 @@
 #include "SkipList.h"
 
-#include <time>
-
 #include <iostream>
 using namespace std;
 
@@ -15,17 +13,9 @@ SkipListNode::~SkipListNode()
 {
 }
 
-void SkipListNode::printAll() // prints all nodes
+void SkipListNode::printNode() // prints a node
 {
-    SkipListNode* temp = this->getNext();
-    while (temp != NULL)
-    {
-        cout << temp->getId() << " ";
-        temp = temp->getNext();
-        if (temp->getNext() == NULL)
-            break;
-    }
-    cout << endl;
+    cout << "[" << id << "," << this << "," << this->getBelow() << "]";
 }
 
 // SKIP_LIST_LAYER
@@ -43,12 +33,22 @@ SkipListLayer::SkipListLayer(SkipListLayer* belowLayer)
     {
         this->layer = belowLayer->getLayer() + 1;
         this->belowLayer = belowLayer;
-        this->firstNode = belowLayer->firstNode;
-        this->lastNode = belowLayer->lastNode;
+        this->lastNode = new SkipListNode(10000, belowLayer->lastNode, NULL);         // 10000 because the ids are [0,9999]
+        this->firstNode = new SkipListNode(-1, belowLayer->firstNode, this->lastNode); // -1 because the ids are [0,9999]
     }
 }
 SkipListLayer ::~SkipListLayer()
 {
+}
+
+void SkipListLayer::printLayer()
+{
+    SkipListNode* tmp = this->firstNode;
+    while (tmp != NULL)
+    {
+        tmp->printNode();
+        tmp = tmp->getNext();
+    }
 }
 
 //SKIP_LIST
@@ -58,12 +58,34 @@ SkipList::SkipList(LinkedListStringNode* virus) : virus(virus)
     this->next = NULL;
     this->vaccinated = new SkipListLayer(NULL);
     this->nonVaccinated = new SkipListLayer(NULL);
-    this->vaccinatedLayers = 1;
-    this->nonVaccinatedLayers = 1;
 }
 
 SkipList::~SkipList()
 {
+}
+
+void SkipList::printNonVaccinated()
+{
+    SkipListLayer* tmp = this->nonVaccinated;
+    while (tmp != NULL)
+    {
+        tmp->printLayer();
+        cout << endl;
+        tmp = tmp->getBelowLayer();
+    }
+    cout << endl;
+}
+
+void SkipList::printVaccinated()
+{
+    SkipListLayer* tmp = this->vaccinated;
+    while (tmp != NULL)
+    {
+        tmp->printLayer();
+        cout << endl;
+        tmp = tmp->getBelowLayer();
+    }
+    cout << endl;
 }
 
 void SkipList::insertVirus(LinkedListStringNode* virus) // inserts at the end
@@ -71,7 +93,7 @@ void SkipList::insertVirus(LinkedListStringNode* virus) // inserts at the end
     SkipList* tmp = this;
     while (tmp->next != NULL)
         tmp = tmp->getNext();
-    tmp->setNext(new Skiplist(virus));
+    tmp->setNext(new SkipList(virus));
 }
 
 SkipList* SkipList::findVirus(LinkedListStringNode* virus)
@@ -79,7 +101,7 @@ SkipList* SkipList::findVirus(LinkedListStringNode* virus)
     SkipList* tmp = this;
     while (tmp != NULL)
     {
-        if (tmp->getVirusName.compare(virus->name) == 0)
+        if (tmp->getVirusName().compare(virus->name) == 0)
             return tmp;
         tmp = tmp->getNext();
     }
@@ -91,7 +113,7 @@ SkipList* SkipList::findVirus(string virusName)
     SkipList* tmp = this;
     while (tmp != NULL)
     {
-        if (tmp->getVirusName.compare(virusName) == 0)
+        if (tmp->getVirusName().compare(virusName) == 0)
             return tmp;
         tmp = tmp->getNext();
     }
@@ -100,56 +122,15 @@ SkipList* SkipList::findVirus(string virusName)
 
 void SkipList::insertNodeVaccinated(int id)
 {
-    SkipListNode* currentNode = this->firstNode;
+    SkipListNode* currentNode = this->getVaccinated()->getFirstNode();
     SkipListNode* nextNode = currentNode->getNext();
-    int currentLayer = this->getLayer();
+    int currentLayer = this->getVaccinated()->getLayer();
 
-    struct timeval time;
-    gettimeofday(&time, NULL);
-    srand((time.tv_nsec * 1000) + (time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-    int layerOfNewNode = 0;
-    while ((rand() % 2) && (currentLayer >= layerOfNewNode))
-    {
-        layerOfNewNode++;
-        if (layerOfNewNode == 8) // 9 layers is the max we want
-            break;
-    }
-
-    if (layerOfNewNode > currentLayer) // checking if i have to create a new layer
-        SkipListLayer* newLayer = new SkipListLayer(this);
-
-    while (1) // until i find the node or i rich the bottom layer (layer 0 and i have not found the id)
-    {
-        if (nextNode->getId() < id) // if the next node id is smaller than the id
-        {
-            currentNode = nextNode;
-            nextNode = currentNode->getNext();
-        }
-        else if (nextNode->getId() > id) // if the next node id is greater than the id
-        {
-            if (currentNode->getBelow() != NULL) // checking if i have another level below, if i have go below
-            {
-                currentNode = currentNode->getBelow();
-                nextNode = currentNode->getNext();
-            }
-            else // i have not another level below
-                return NULL;
-        }
-        else // the next node id is equal to the id
-            return nextNode;
-    }
-}
-
-void SkipList::insertNodeNonVaccinated(int id)
-{
-    SkipListNode* currentNode = this->getNonVaccinated()->getFirstNode();
-    SkipListNode* nextNode = currentNode->getNext();
-    int currentLayer = this->getNonVaccinated()->getLayer();
-
-    struct timeval time;
-    gettimeofday(&time, NULL);
-    srand((time.tv_nsec * 1000) + (time.tv_sec * 1000) + (time.tv_usec / 1000));
+    // struct timeval time;
+    // gettimeofday(&time, NULL);
+    struct timespec time;
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    srand((time.tv_nsec * 1000) + (time.tv_sec * 1000));
 
     int layerOfNewNode = 0;
     while ((rand() % 2) && (currentLayer >= layerOfNewNode))
@@ -161,8 +142,11 @@ void SkipList::insertNodeNonVaccinated(int id)
 
     if (layerOfNewNode > currentLayer) // checking if i have to create a new layer
     {
-        SkipListLayer* newLayer = new SkipListLayer(this->nonVaccinated);
-        this->nonVaccinated = newLayer;
+        SkipListLayer* newLayer = new SkipListLayer(this->vaccinated);
+        this->vaccinated = newLayer;
+        currentLayer++;
+        currentNode = this->vaccinated->getFirstNode();
+        nextNode = currentNode->getNext();
     }
 
     SkipListNode* tmp = NULL;
@@ -175,9 +159,9 @@ void SkipList::insertNodeNonVaccinated(int id)
         }
         else if (nextNode->getId() > id) // if the next node id is greater than the id
         {
-            if (layerOfNewNode <= currentLayer)
+            if (layerOfNewNode >= currentLayer)
             {
-                SkipListNode* newNode = new SkipListNode(id);
+                SkipListNode* newNode = new SkipListNode(id, NULL, NULL);
                 currentNode->setNext(newNode);
                 newNode->setNext(nextNode);
                 if (tmp != NULL)
@@ -193,94 +177,105 @@ void SkipList::insertNodeNonVaccinated(int id)
                 currentLayer--;
             }
             else // i have not another level below
+                return;
+        }
+        else // the next node id is equal to the id
+            return;
+    }
+}
+
+void SkipList::insertNodeNonVaccinated(int id)
+{
+    SkipListNode* currentNode = this->getNonVaccinated()->getFirstNode();
+    SkipListNode* nextNode = currentNode->getNext();
+    int currentLayer = this->getNonVaccinated()->getLayer();
+
+    // struct timeval time;
+    // gettimeofday(&time, NULL);
+    struct timespec time;
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    srand((time.tv_nsec * 1000) + (time.tv_sec * 1000));
+
+    int layerOfNewNode = 0;
+    while ((rand() % 2) && (currentLayer >= layerOfNewNode))
+    {
+        layerOfNewNode++;
+        if (layerOfNewNode == 8) // 9 layers is the max we want
+            break;
+    }
+
+    if (layerOfNewNode > currentLayer) // checking if i have to create a new layer
+    {
+        SkipListLayer* newLayer = new SkipListLayer(this->nonVaccinated);
+        this->nonVaccinated = newLayer;
+        currentLayer++;
+        currentNode = this->nonVaccinated->getFirstNode();
+        nextNode = currentNode->getNext();
+    }
+
+    SkipListNode* tmp = NULL;
+    while (1) // until i find the node or i rich bottom layer (layer 0 and i have not found the id)
+    {
+        if (nextNode->getId() < id) // if the next node id is smaller than the id
+        {
+            currentNode = nextNode;
+            nextNode = currentNode->getNext();
+        }
+        else if (nextNode->getId() > id) // if the next node id is greater than the id
+        {
+            if (layerOfNewNode >= currentLayer)
+            {
+                SkipListNode* newNode = new SkipListNode(id, NULL, NULL);
+                currentNode->setNext(newNode);
+                newNode->setNext(nextNode);
+                if (tmp != NULL)
+                {
+                    tmp->setBelow(newNode);
+                }
+                tmp = newNode;
+            }
+            if (currentNode->getBelow() != NULL) // checking if i have another level below, if i have go below
+            {
+                currentNode = currentNode->getBelow();
+                nextNode = currentNode->getNext();
+                currentLayer--;
+            }
+            else // i have not another level below
+                return;
+        }
+        else // the next node id is equal to the id
+            return;
+    }
+}
+
+void SkipList::deleteNodeVaccinated(int id)
+{
+    SkipListNode* currentNode = this->getVaccinated()->getFirstNode();
+    SkipListNode* nextNode = currentNode->getNext();
+
+    while (1) // until i find the node or i rich bottom layer (layer 0 and i have not found the id)
+    {
+        if (nextNode->getId() < id) // if the next node id is smaller than the id
+        {
+            currentNode = nextNode;
+            nextNode = currentNode->getNext();
+        }
+        else if (nextNode->getId() > id) // if the next node id is greater than the id
+        {
+            if (currentNode->getBelow() != NULL) // checking if i have another level below, if i have go below
+            {
+                currentNode = currentNode->getBelow();
+                nextNode = currentNode->getNext();
+            }
+            else // i have not another level below
                 return NULL;
         }
         else // the next node id is equal to the id
-            return nextNode;
-    }
-}
-
-void nikolas()
-{
-    skipListLevel* currLevel = this->getCeiling();
-    skipListNode* currNode = currLevel->getNegInf();
-    skipListNode* nextNode = currNode->getNext();
-
-    skipListNode* prevCreated = NULL;
-    int create = 0;
-    int heightOfNewNode = 0;
-    while (this->coinFlip() && (this->getCeiling()->getMyLevel() >= heightOfNewNode) && this->getCeiling()->getMyLevel() < maxLevels - 1)
-    {
-        heightOfNewNode++;
-    }
-
-    while (1)
-    {
-        if (id < nextNode->getId())
         {
-            if (currLevel->getMyLevel() <= heightOfNewNode)
-            { // create a node
-                if (create)
-                {
-                    skipListNode* downNode = new skipListNode(citizen);
-                    checkNew(downNode);
-                    prevCreated->setDown(downNode);
-                    currNode->setNext(downNode);
-                    downNode->setNext(nextNode);
-                    prevCreated = downNode;
-                }
-                else if (!create) // if it is the first time
-                {
-                    prevCreated = new skipListNode(citizen);
-                    checkNew(prevCreated);
-                    currNode->setNext(prevCreated);
-                    prevCreated->setNext(nextNode);
-                    if (currLevel == this->getCeiling() && heightOfNewNode == this->getCeiling()->getMyLevel() + 1)
-                    { // make a new level
-                        skipListLevel* newLevel = new skipListLevel(this->getCeiling(), this->getCeiling()->getMyLevel() + 1);
-                        checkNew(newLevel);
-                        this->setCeiling(newLevel);
-                        skipListNode* newN = new skipListNode(prevCreated);
-                        checkNew(newN);
-                        this->getCeiling()->getList()->add(newN);
-                    }
-                    create = 1;
-                }
-            }
+            SkipListNode* nodeToDelete = nextNode;
 
-            if (currNode->getDown() != NULL)
-            { // if there is a down level
-                // step down
-                currNode = currNode->getDown();
-                nextNode = currNode->getNext();
-                currLevel = currLevel->getDownLevel();
-            }
-            else
-            {
-                if (!create)
-                {
-                    prevCreated = new skipListNode(citizen);
-                    checkNew(prevCreated);
-                    currNode->setNext(prevCreated);
-                    prevCreated->setNext(nextNode);
-                }
-                return;
-            }
-        }
-        else if (id > nextNode->getId())
-        {
-            currNode = nextNode;
-            nextNode = nextNode->getNext();
-        }
-        else if (id == nextNode->getId())
-        {
-            return;
         }
     }
-}
-
-void SkipList::deleteNodeVaccinated(int)
-{
 }
 
 void SkipList::deleteNodeNonVaccinated(int)
